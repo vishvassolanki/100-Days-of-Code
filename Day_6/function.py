73daa4381b9696ab92ba6801a425865390afe2dd
vishvassolanki <vishvassolanki08@gmail.com>
2025-07-26 14:44:03 +0530

    Day 6 complete

---
+++ Day_6/function.py
@@ -0,0 +1,8 @@
+# print("Hello")
+# num_char = len("Vishvas")
+# print(num_char)
+
+def greet(name):
+    print(f"Hello, {name}")
+
+greet("Vishvas")
---
+++ Day_6/while_loop.py
@@ -0,0 +1,5 @@
+count = 1
+
+while count <= 5:
+    print("Number is:", count)
+    count += 1
