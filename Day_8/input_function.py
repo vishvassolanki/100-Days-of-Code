4244699da2f3bbb1382dc87fbffb2495ec8f3c63
vishvassolanki <vishvassolanki08@gmail.com>
2025-08-01 03:43:47 +0530

    Day 8 complete

---
+++ Day_8/input_function.py
@@ -0,0 +1,26 @@
+# def greet():
+#     print("Namste!")
+#     print("Kem cho?")
+#     print("Jay Mataji")
+#
+# greet()
+
+# def greet(name):
+#     print(f"Hello {name}")
+#
+# greet("Vishvas")
+
+# def greet_with(name, location):
+#     print(f"Hello {name}")
+#     print(f"What is it like in {location}")
+#
+# greet_with("Vishvas", "Gir Somnath")
+# greet_with(greet_with(name, location):
+
+def greet_with(name = "Vishvas", location = "Gir Somnath"):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+greet_with()
+
+
