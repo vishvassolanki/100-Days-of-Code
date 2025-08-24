01644f7309afeb14ed75de563beee6243af8333e
vishvassolanki <vishvassolanki08@gmail.com>
2025-08-24 15:23:33 +0530

    Day 10 compelete

---
+++ Day_10/function.py
@@ -0,0 +1,9 @@
+def format_name(f_name, l_name):
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+
+    return f"{formated_f_name} {formated_l_name}"
+
+formated_string = format_name("viShVas", "solanki")
+# print(formated_string)
+
